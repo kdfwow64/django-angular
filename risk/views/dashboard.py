c57617f91a8d8a56e23ff6342870801d7e514b7f
Varghese Chacko <varghesec@gmail.com>
2018-08-02 11:24:06 +0530

    Complete project 2

--- risk/views/dashboard.py
+++ risk/views/dashboard.py
@@ -24,17 +24,21 @@ def sidebar(request):
     """Sub template for dashboard sidebar."""
     user = request.user
     try:
-        core_account = user.accountmembership_set.filter(id_account_id__name=settings.CORE_ACCOUNT, is_company_viewable=1).get()
+        # core_account = user.accountmembership_set.filter(id_account_id__name=settings.CORE_ACCOUNT, is_company_viewable=1).get()
+        core_account = user.accountmembership_set.filter(id_account_id__name=settings.CORE_ACCOUNT).get()
         # Can see everything
-        companies = [{'id': c.id, 'name': c.name} for c in Company.objects.filter(Q(is_active=True)).all()]
+        companies = [{'id': c.id, 'name': c.name} for c in Company.objects.filter(Q(is_active=True)).order_by('name').all()]
     except:
+        company_dict = {c.id_company.id: c.id_company.name for c in user.companymember_set.filter(Q(is_active=True)).all()}
         try:
-            admin_account_membership = user.accountmembership_set.filter(is_admin=1, is_company_viewable=1).all()
+            # admin_account_membership = user.accountmembership_set.filter(is_admin=1, is_company_viewable=1).all()
+            admin_account_membership = user.accountmembership_set.filter(is_admin=1).all()
             companies = []
             for account_membership in admin_account_membership:
-                companies += [{'id': c.id, 'name': c.name} for c in account_membership.id_account.account_company.filter(Q(is_active=True)).all()]
+                company_dict.update({c.id: c.name for c in account_membership.id_account.account_company.filter(Q(is_active=True)).all()})
         except:
-            companies = [{'id': c.id_company.id, 'name': c.id_company.name} for c in request.user.companymember_set.filter(Q(is_active=True)).all()]
+            pass
+        companies = [{'id': key, 'name': company_dict[key]} for key in sorted(company_dict, key=company_dict.get, reverse=False)]
     return render(request, 'dashboard/subtemplate/sidebar.html', context={'companies': companies})
 
 @login_required
