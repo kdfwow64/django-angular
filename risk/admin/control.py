2ef3ef1f987e9df3013dc7896881768850d7d02e
Varghese Chacko <varghesec@gmail.com>
2018-11-16 19:46:31 +0530

    Fix Admin control forms

--- risk/admin/control.py
+++ risk/admin/control.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django import forms
 from django.contrib.admin.widgets import FilteredSelectMultiple
+
 # , ControlCategoryCategory
-from risk.models.control import ControlCategoryNotification, ControlCategoryControl, ControlCategoryCIATriad
+from risk.models.control import Control, ControlCategory, ControlCategoryNotification, ControlCategoryControl, ControlCategoryCIATriad
 from django.contrib.auth.forms import (
     UserChangeForm, UserCreationForm,
 )
@@ -16,12 +17,12 @@ class ControlCategoryNotificationInline(admin.TabularInline):
               'is_active', 'is_deleted', 'created_by')
 
 
-class ControlCategoryControlInline(admin.TabularInline):
+# class ControlCategoryControlInline(admin.TabularInline):
 
-    model = ControlCategoryControl
-    extra = 1
-    fields = ('id_controlcategory',
-              'is_active', 'is_deleted', 'created_by')
+#     model = ControlCategoryControl
+#     extra = 1
+#     fields = ('id_controlcategory',
+#               'is_active', 'is_deleted', 'created_by')
 
 
 class ControlCategoryCIATriadInline(admin.TabularInline):
@@ -32,8 +33,48 @@ class ControlCategoryCIATriadInline(admin.TabularInline):
               'is_active', 'is_deleted', 'created_by')
 
 
-class ControlAdmin(admin.ModelAdmin):
+class ControlAdminForm(forms.ModelForm):
+    control_category = forms.ModelMultipleChoiceField(
+        queryset=ControlCategory.objects.order_by('name').all(),
+        required=False,
+        widget=FilteredSelectMultiple(
+            verbose_name='Control Categories',
+            is_stacked=False
+        )
+    )
 
+    class Meta:
+        model = Control
+        fields = ('name', 'model_number', 'abbrv', 'description', 'vendor')
+
+    def __init__(self, *args, **kwargs):
+        super(ControlAdminForm, self).__init__(*args, **kwargs)
+        if self.instance.pk:
+            self.fields['control_category'].initial = self.instance.categories.all()
+            self.fields['control_category'].queryset = self.fields['control_category'].queryset.exclude(pk=self.instance.pk)
+
+    def save(self, commit=True):
+        control = super(ControlAdminForm, self).save(commit=False)
+        if commit:
+            control.save()
+
+        if control.pk:
+            control_category = self.cleaned_data['control_category']
+            print(control_category)
+            for category in control.categories.all():
+                if category in control_category.all(): # Already in category, no need to handle
+                    control_category = control_category.exclude(pk=category.pk)
+                else: # Remove if removed in front-end
+                    ControlCategoryControl.objects.get(id_control=control, id_controlcategory=category).delete()
+
+            for category in control_category.all(): # Add new categories
+                ControlCategoryControl.objects.create(id_control=control, id_controlcategory=category)
+
+        return control
+
+
+class ControlAdmin(admin.ModelAdmin):
+    form = ControlAdminForm
     readonly_fields = ('date_created', 'created_by', 'date_modified', 'modified_by',
                        'date_deleted', 'deleted_by', 'date_deactivated', 'deactivated_by',
                        )
@@ -41,13 +82,13 @@ class ControlAdmin(admin.ModelAdmin):
     autocomplete_fields = ['vendor', ]
     fieldsets = (
         ('Control Specific', {
-         'fields': ('name', 'model_number', 'abbrv', 'description', 'vendor',)}),
+         'fields': ('name', 'model_number', 'abbrv', 'description', 'vendor','control_category')}),
         ('Management Detail', {
             'classes': ('grp-collapse grp-closed',),
             'fields': ('company', ('is_active', 'is_deleted',), ('date_created', 'created_by',), ('date_modified', 'modified_by',), ('date_deleted', 'deleted_by',), ('date_deactivated', 'deactivated_by',))}),
     )
     list_select_related = []
-    inlines = (ControlCategoryControlInline,)
+    # inlines = (ControlCategoryControlInline,)
     list_display = (
         'id',
         'name',
